e0933ba8e1b8e0936271b4d8f65bd6b452809552
Julien Ruaux <jruaux@jrx.home.ruaux.org>
2018-09-03 01:49:29 -0700

    added material cards

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchAlbumsComponent } from './search-albums/search-albums.component';
 
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatCardModule, MatInputModule, MatListModule, MatGridListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -21,10 +21,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
+    FlexLayoutModule,
     MatButtonModule,
+    MatIconModule,
     MatCardModule,
     MatInputModule,
     MatListModule,
+    MatGridListModule,
     MatToolbarModule
   ],
   providers: [],
